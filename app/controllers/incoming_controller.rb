34b56b75e6ee4f550b6b24742d482c1a6b7fd2be
System Administrator <mattwhite1337@gmail.com>
2015-09-03 22:16:56 -0400

    Bookmark creation

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -20,11 +20,10 @@ class IncomingController < ApplicationController
 
     # Check if the topic is nil, if so, create and save a new topic
     if @topic == nil
-      Topic.create(title: params[:subject], user_id: @user.id)
+      @topic = Topic.create(title: params[:subject], user_id: @user.id)
     end
     # Now that you're sure you have a valid user and topic, build and save a new bookmark
-
-
+    Bookmark.create(url: @url, topic: @topic.id)
     # Assuming all went well. 
     head 200
   end
