28da54bd849463925e7ece68cf4cedc0879becd7
System Administrator <mattwhite1337@gmail.com>
2015-09-03 20:55:09 -0400

    parse email and create topic

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,20 +5,23 @@ class IncomingController < ApplicationController
 
   def create
 
-     # Take a look at these in your server logs
-     # to get a sense of what you're dealing with.
-     #puts "INCOMING PARAMS HERE: #{params}"
-     puts params["body-plain"]
+    # Take a look at these in your server logs
+    # to get a sense of what you're dealing with.
+    #puts "INCOMING PARAMS HERE: #{params}"
+    #puts params["body-plain"]
+
     # Find the user by using params[:sender]
     @user = User.find_by(email: params[:sender])
     # Find the topic by using params[:subject]
     @topic = Topic.find_by(title: params[:subject])
     # Assign the url to a variable after retreiving it from params["body-plain"]
-    
+    @url = params["body-plain"]
     # Check if user is nil, if so, create and save a new user
 
     # Check if the topic is nil, if so, create and save a new topic
-
+    if @topic = nil
+      Topic.create(title: @topic.title, user: @user.id)
+    end
     # Now that you're sure you have a valid user and topic, build and save a new bookmark
 
 
