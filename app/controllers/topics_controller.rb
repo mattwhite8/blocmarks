9d10663bcdbcd5684e295800c682571e36fdecfb
System Administrator <mattwhite1337@gmail.com>
2015-09-05 11:54:23 -0400

    Change to application.html.erb

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,47 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def edit
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    @topic.user = current_user
+    if @topic.save
+      redirect_to topics_path, notice: "Topic was saved successfully."
+    else
+      flash[:error] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    title = @topic.title
+
+    if @topic.destroy 
+      flash[:notice] = "\"#{title}\" was deleted sucessfully."
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deleting the post."
+      redirect_to :show
+    end
+
+  end
+
+  private
+  
+  def topic_params
+    params.require(:topic).permit(:title, :user_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+ resources :topics
+
   devise_for :users
+
   get 'welcome/about'
 
   post :incoming, to: 'incoming#create'
